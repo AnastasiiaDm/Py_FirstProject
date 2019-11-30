4051a480ef07d24888d948eb009bc7c2a25a9c4e
AnastasiiaDm <37839136+AnastasiiaDm@users.noreply.github.com>
2019-12-01 00:44:21 +0200

    first

---
+++ first_app.py
@@ -0,0 +1,21 @@
+print('\n'
+
+    '*\t\t\t*\t\t\t*\t\t\t**********  ***********  *************\n'
+      '*\t\t\t*\t\t   * *   \t\t*\t\t\t\t *\t\t *\t\t\t *\n'
+      '*\t\t\t*\t\t  *   *   \t\t*\t\t\t\t *\t\t *\t\t\t *\n'
+      '*\t\t\t*\t\t *     *   \t\t*\t\t\t\t *\t\t *************\n'
+      '*************\t\t*       *\t\t*\t\t\t\t *\t\t\t\t\t *\n'
+      '*\t\t\t*\t   ***********\t\t*\t\t\t\t *\t\t \t\t  *  *\n'
+      '*\t\t\t*\t  *           *\t\t*\t\t\t\t *\t\t \t   *     *\n'
+      '*\t\t\t*\t *             *\t*\t\t\t\t *\t\t\t*\t\t *\n'
+      '*\t\t\t*\t*               *\t************\t *\t\t *\t\t\t *\n'
+    )
+print(
+    '\\a''\t\t''Bell(alert)''\a''\n'
+    '\\b''\t\t''Backspace''B\b''\n'
+    '\\n''\t\t''New line' '\n'
+    '\\t''\t\t''Horizontal tab''\t''\n'
+    '\\''\\''\t\t''Backslash'' \\''\n'
+    '\\''\"''\t\t' 'Double question mark \"\n'
+    '\\\'\t\tSingle question mark \''
+    )
